797488c27420799438d1c9619051ed156d8bfa5c
Venkata Pavan <venkatapavan2905@gmail.com>
2025-06-29 19:34:27 +0530

    updated provider with remote backend

--- AZ_ResourceGroup.tf
+++ AZ_ResourceGroup.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.8.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "terraform-rg"
+    storage_account_name = "terraform-SA"
+    container_name = "terraform-container"
+    key = "azure-backend.tfstate"
+    
+  }
   required_version = ">=1.9.0"
 }
 

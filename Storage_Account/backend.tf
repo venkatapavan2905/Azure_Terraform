05da6793148ec57fad6ec3b544dcf8df3aa55a35
Venkata Pavan <venkatapavan2905@gmail.com>
2025-07-02 10:29:39 +0530

    updated key attribute

--- Storage_Account/backend.tf
+++ Storage_Account/backend.tf
@@ -1,9 +1,9 @@
 terraform {
     backend "azurerm" {
        resource_group_name = "terraform-state-rg"
-       storage_account_name = "terraform20149"
+       storage_account_name = "terraform2777"
        container_name = "tfstate"
-       key = "azure-backend.tfstate"
+       key = "sa.tfstate"
   }
 
 }

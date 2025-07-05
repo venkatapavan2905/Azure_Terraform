d4c5cca42f3e9e474c9b63b0aad01fc67d5bb4c1
Venkata Pavan <venkatapavan2905@gmail.com>
2025-07-05 17:38:49 +0530

    variables.tf

--- DataTypes/variables.tf
+++ DataTypes/variables.tf
@@ -1,5 +1,28 @@
 variable "digit" {
     type = number
     default = 22
-  
 }
+
+variable "word" {
+    type = string
+    default = "Hello World !!"
+}
+
+variable "boolean" {
+  type = bool
+  default = false
+}
+
+variable "listed" {
+  type = list()
+  default = ["This", "is", "From", "Terraform"]
+}
+
+variable "mapped" {
+  type = map()
+  default = {
+    "key" = "value",
+    "key2" = "value2"
+  }
+}
+
